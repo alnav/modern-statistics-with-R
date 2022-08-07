5da41ee434004ada30f784667f6ee8f4739cdee9
Alessio Navarra <alessio.navarra@gmail.com>
2022-08-07 06:18:45 +0100

    Create ex 2.4-2.8.R

---
+++ ex 2.4-2.8.R
@@ -0,0 +1,18 @@
+# exercise 2.4
+height <- c(176, 175, 185, 164)
+weight <- c(80, 75, 89, 55)
+people <- data.frame(weight, height)
+
+# exercise 2.5
+x <- 1:5
+y <- 4:1
+z <- c(x, y)
+
+# exercise 2.6
+mean(height)
+cor(height, weight)
+
+# exercise 2.8
+sqrt(pi)
+exp(2) * log(4)
+
